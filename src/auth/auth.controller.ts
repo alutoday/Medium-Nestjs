33e2774099c3b3c6e0df7bd99fd6268e72de3c17
alutoday <anhnn1201@gmail.com>
2025-10-28 16:07:03 +0700

    update @Throttle -pass sunlint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { Throttle } from '@nestjs/throttler';
 import { AuthService } from './auth.service';
 import { LoginDto, RegisterDto } from './dto/auth.dto';
 
@@ -11,9 +12,10 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
+  
+  @Throttle({ default: { ttl: 60_000, limit: 5 } }) 
   @Post('login')
   login(@Body('user') dto: LoginDto) {
     return this.authService.login(dto);
   }
-  
 }
