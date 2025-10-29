a9f4a227ed8df7732944e4cdb2170a9053134274
alutoday <anhnn1201@gmail.com>
2025-10-29 16:10:34 +0700

    add cru-user function

---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,24 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  username?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;  
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  image?: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserById(userId: number) {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+    if (!user) throw new NotFoundException('User is not found');
+
+    return this.excludePrivateInfo(user);
+  }
+
+  async updateUser(userId: number, updateUserDto: UpdateUserDto) {
+    if (updateUserDto.password) {
+      updateUserDto.password = await bcrypt.hash(updateUserDto.password, 10);
+    }
+
+    const updated = await this.prisma.user.update({
+      where: { id: userId },
+      data: updateUserDto,
+    });
+
+    return this.excludePrivateInfo(updated);
+  }
+
+  private excludePrivateInfo(user: User) {
+    const { id, password, createdAt, updatedAt, ...rest } = user;
+    return rest;
+  }
 }
