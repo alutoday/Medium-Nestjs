218c4fa4baffaf31847dd74f4b6a2aa565a34b46
alutoday <anhnn1201@gmail.com>
2025-11-02 18:16:55 +0700

    add delete article function

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -18,11 +18,12 @@ import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { RequestUser } from 'src/auth/request-user.interface';
 import { ArticleEntity } from './entities/article.entity';
 import { JwtAuthGuardDetect } from 'src/auth/auth.guard.detect';
+import { UpdateArticleDto } from './dto/update-article.dto';
 
 
 @Controller('api/articles')
 export class ArticleController {
-  constructor(private readonly articlesService: ArticleService) {}
+  constructor(private readonly articleService: ArticleService) {}
 
   @UseGuards(JwtAuthGuard)
   @Post()
@@ -30,7 +31,7 @@ export class ArticleController {
     @Body('article') dto: CreateArticleDto,
     @Req() req: RequestUser & Request
   ): Promise<ArticleEntity> {
-    const article = await this.articlesService.create(req.user.sub, dto);
+    const article = await this.articleService.create(req.user.sub, dto);
     return new ArticleEntity(article);
   } 
 
@@ -38,7 +39,25 @@ export class ArticleController {
   @Get(':slug')
   async findOne(@Param('slug') slug: string, @Req() req: RequestUser & Request): Promise<ArticleEntity> {
     const userId = req.user?.sub;
-    const article = await this.articlesService.findBySlug(slug, userId);
+    const article = await this.articleService.findBySlug(slug, userId);
     return new ArticleEntity(article);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':slug')
+  async update(
+    @Param('slug') slug: string,
+    @Body('article') dto: UpdateArticleDto,
+    @Req() req: RequestUser & Request,
+  ) {
+    const article = await this.articleService.update(slug, req.user.sub, dto);
+    return new ArticleEntity(article);
+  }
+
+    @UseGuards(JwtAuthGuard)
+  @Delete(':slug')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('slug') slug: string, @Req() req: RequestUser & Request) {
+    return this.articleService.delete(slug, req.user.sub);
   }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,11 +1,13 @@
 import {
   Injectable,
   NotFoundException,
+  ForbiddenException,
 } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import slugify from 'slugify';
 import { Article, Tag, User } from '@prisma/client';
+import { UpdateArticleDto } from './dto/update-article.dto';
 
 type FullArticle = Article & {
   tagList: Tag[];
@@ -86,5 +88,77 @@ export class ArticleService {
   async countFavorites(articleId: number): Promise<number> {
     return this.prisma.favoriteArticles.count({ where: { articleId } });
   }
+
+  async update(
+    slug: string,
+    userId: number,
+    dto: UpdateArticleDto,
+  ): Promise<FullArticle> {
+    const existing = await this.prisma.article.findUnique({
+      where: { slug },
+      include: { tagList: true },
+    });
+
+    if (!existing) {
+      throw new NotFoundException('Article not found');
+    }
+    if (existing.authorId !== userId) {
+      throw new ForbiddenException('Unauthorized');
+    }
+
+    let newSlug = slug;
+    if (dto.title) {
+      newSlug = this.generateSlug(dto.title);
+    }
+
+    let tagListUpdate = {};
+    if (dto.tagList) {
+      const newTags = dto.tagList.map((tag) => ({
+        where: { name: tag },
+        create: { name: tag },
+      }));
+      tagListUpdate = {
+        tagList: {
+          set: [],
+          connectOrCreate: newTags,
+        },
+      };
+    }
+
+    const updated = await this.prisma.article.update({
+      where: { slug },
+      data: {
+        title: dto.title,
+        description: dto.description,
+        body: dto.body,
+        slug: newSlug,
+        ...tagListUpdate,
+      },
+      include: {
+        author: true,
+        tagList: true,
+      },
+    });
+
+    return this.buildSingleArticleResponse(updated, userId);
+  }
+
+  async delete(slug: string, userId: number) {
+    const article = await this.prisma.article.findUnique({ where: { slug } });
+    if (!article) {
+      throw new NotFoundException('Article not found');
+    }
+    if (article.authorId !== userId) {
+      throw new ForbiddenException('Unauthorized');
+    }
+    await this.prisma.favoriteArticles.deleteMany({
+      where: { articleId: article.id },
+    });
+    await this.prisma.article.delete({ where: { slug } });
+    return;
+  }
+
+
+  
   
 }
