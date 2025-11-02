149855cfe502a68775773f30e407f1eb368cade8
alutoday <anhnn1201@gmail.com>
2025-11-03 01:24:29 +0700

    add follow function

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Delete, Param, UseGuards, Req } from '@nestjs/co
 import {JwtAuthGuardDetect } from 'src/auth/auth.guard.detect';
 import { ProfileService } from './profile.service';
 import { ProfileEntity } from './entities/profile.entity';
+import { JwtAuthGuard } from 'src/auth/auth.guard';
 
 @Controller('api/profiles')
 export class ProfileController {
@@ -10,9 +11,17 @@ export class ProfileController {
   @Get(':username')
   @UseGuards(JwtAuthGuardDetect)
   async get(@Param('username') username: string, @Req() req: any) {
-    const viewerId: number | undefined = req?.user?.sub;
-    const data = await this.service.getProfile(username, viewerId);
+    const userId: number | undefined = req?.user?.sub;
+    const data = await this.service.getProfile(username, userId);
     return new ProfileEntity(data);
   }
+
+  @Post(':username/follow')
+  @UseGuards(JwtAuthGuard)
+  async follow(@Param('username') username: string, @Req() req: any) {
+    const data = await this.service.follow(username, req.user.sub);
+    return new ProfileEntity(data);
+  }
+
  
 }
