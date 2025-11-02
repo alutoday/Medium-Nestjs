bfcf5291b30bd1562fca00f2f4a7de38fef0803e
alutoday <anhnn1201@gmail.com>
2025-11-02 18:01:09 +0700

    add get article function

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -17,6 +17,8 @@ import { CreateArticleDto } from './dto/create-article.dto';
 import { JwtAuthGuard } from 'src/auth/auth.guard';
 import { RequestUser } from 'src/auth/request-user.interface';
 import { ArticleEntity } from './entities/article.entity';
+import { JwtAuthGuardDetect } from 'src/auth/auth.guard.detect';
+
 
 @Controller('api/articles')
 export class ArticleController {
@@ -31,4 +33,12 @@ export class ArticleController {
     const article = await this.articlesService.create(req.user.sub, dto);
     return new ArticleEntity(article);
   } 
+
+  @UseGuards(JwtAuthGuardDetect)
+  @Get(':slug')
+  async findOne(@Param('slug') slug: string, @Req() req: RequestUser & Request): Promise<ArticleEntity> {
+    const userId = req.user?.sub;
+    const article = await this.articlesService.findBySlug(slug, userId);
+    return new ArticleEntity(article);
+  }
 }
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,10 +1,18 @@
 import {
   Injectable,
+  NotFoundException,
 } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import slugify from 'slugify';
+import { Article, Tag, User } from '@prisma/client';
 
+type FullArticle = Article & {
+  tagList: Tag[];
+  author: User;
+  favorited?: boolean;
+  favoritesCount?: number;
+};
 
 @Injectable()
 export class ArticleService {
@@ -41,6 +49,42 @@ export class ArticleService {
     });
 
     return article;
+  }
+
+  async findBySlug(slug: string, userId?: number): Promise<FullArticle> {
+    const article = await this.prisma.article.findUnique({
+      where: { slug },
+      include: {
+        author: true,
+        tagList: true,
+      },
+    });
+    if (!article) throw new NotFoundException('Article not found');
+
+    return this.buildSingleArticleResponse(article, userId);
+  }
+
+  async buildSingleArticleResponse(
+    article: FullArticle,
+    userId?: number,
+  ): Promise<FullArticle> {
+    const [favorited, favoritesCount] = await Promise.all([
+      userId ? this.isFavorited(userId, article.id) : false,
+      this.countFavorites(article.id),
+    ]);
+
+    return { ...article, favorited, favoritesCount };
+  }
+
+  async isFavorited(userId: number, articleId: number): Promise<boolean> {
+    const fav = await this.prisma.favoriteArticles.findFirst({
+      where: { userId, articleId },
+    });
+    return !!fav;
+  }
+
+  async countFavorites(articleId: number): Promise<number> {
+    return this.prisma.favoriteArticles.count({ where: { articleId } });
   }
   
 }
